48d05b2d7dc0fbcaaf30827a62c50cacc1d51ddd
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-07-02 11:48:01 +1200

    javascript equals test

---
+++ script.js
@@ -0,0 +1,17 @@
+var house = 'tree';
+var number = 2;
+
+var test = 0;
+var test2 = 0;
+
+test == house;
+
+console.log(test + ' double equals output');
+
+test === house; 
+
+console.log(test + ' triple equals output');
+
+test = house; 
+
+console.log(test + ' single equals output');
